805d564736877c081fba6d626388ba0a2967f9a6
USER <castela0119@gmail.com>
2022-11-30 11:33:27 +0900

    2.2 Hello JPA - 애플리케이션 개발 - 정석적인 코드

---
+++ src/main/java/org/example/hellojpa/JpaMain.java
@@ -0,0 +1,44 @@
+package org.example.hellojpa;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+
+public class JpaMain {
+    public static void main(String[] args) {
+
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
+
+        // 데이터베이스 커넥션 받았다고 생각할 것
+        EntityManager em = emf.createEntityManager();
+
+        // 트랜젝션을 호출하고 begin 으로 데이터베이스 트랜젝션을 시작
+        EntityTransaction tx = em.getTransaction();
+        tx.begin();
+
+        try {
+            Member member = new Member();
+            member.setId(2L);
+            member.setName("HelloB");
+
+            em.persist(member);
+
+            // commit 으로 트랜젝션을 끝냄
+            tx.commit();
+
+        } catch (Exception e) {
+
+            tx.rollback();
+
+        } finally {
+
+            // 사용을 다하고나면 EntityManager 를 꼭 닫아줘야 한다.
+            em.close();
+
+        }
+
+        // 전체 어플리케이션이 끝나면 EntitiyFactoryManager 까지 닫아주자.
+        emf.close();
+    }
+}
