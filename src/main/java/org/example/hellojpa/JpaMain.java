75beaf49ced1e2199ebda7a4403d29d97ff8e196
castela0119 <78953029+castela0119@users.noreply.github.com>
2022-12-20 10:00:33 +0900

    10.1 소개 - (1) JQPL

--- src/main/java/org/example/hellojpa/JpaMain.java
+++ src/main/java/org/example/hellojpa/JpaMain.java
@@ -21,38 +21,14 @@ public class JpaMain {
         tx.begin();
 
         try {
+            List<Member> result = em.createQuery(
+                    "select m From Member m where m.username like '%kim%'",
+                    Member.class
+            ).getResultList();
 
-            Member member = new Member();
-            member.setUsername("member1");
-            member.setHomeAddress(new Address("homeCity", "street", "zipcode"));
-
-            member.getFavoriteFoods().add("치킨");
-            member.getFavoriteFoods().add("족발");
-            member.getFavoriteFoods().add("피자");
-
-            member.getAddressHistory().add(new AddressEntity("old1", "street", "10000"));
-            member.getAddressHistory().add(new AddressEntity("old2", "street", "10000"));
-
-            em.persist(member);
-
-            em.flush();
-            em.clear();
-
-            System.out.println("==================== START ====================");
-            Member findMember = em.find(Member.class, member.getId());
-
-            // 이렇게 갈아끼워야 합니다!
-            // homeCity --> newCity
-            Address a = findMember.getHomeAddress();
-            findMember.setHomeAddress(new Address("newCity", a.getStreet(), a.getZipcode()));
-
-            // 치킨을 --> 한식으로 바꿔보자. (food 변경)
-            findMember.getFavoriteFoods().remove("치킨");
-            findMember.getFavoriteFoods().add("한식");
-
-            // old1 을 --> new1 으로 바꿔보자. (address 변경)
-            findMember.getAddressHistory().remove(new AddressEntity("old1", "street", "10000"));
-            findMember.getAddressHistory().add(new AddressEntity("newCity1", "street", "10000"));
+            for (Member member : result) {
+                System.out.println("member = " + member);
+            }
 
             tx.commit();
         } catch (Exception e) {
